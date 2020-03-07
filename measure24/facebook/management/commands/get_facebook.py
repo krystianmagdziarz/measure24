a2360f3567bdfd9935313d2fd35d0015f6ad34ce
Krystian Magdziarz <admin@kmagdziarz.pl>
2020-03-07 16:09:57 +0100

    #21 Handle all exception for Sentry

--- measure24/facebook/management/commands/get_facebook.py
+++ measure24/facebook/management/commands/get_facebook.py
@@ -13,54 +13,59 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        for user in FacebookUser.objects.all():
-            facebook = Facebook(user.facebook_login, user.facebook_password, user.pk, headless_mode=False)
-            facebook.login()
+        try:
+            for user in FacebookUser.objects.all():
+                facebook = Facebook(user.facebook_login, user.facebook_password, user.pk, headless_mode=False)
+                facebook.login()
 
-            try:
-                for group in FacebookGroup.objects.filter(active=True, facebook_user=user):
-                    facebook_entries = facebook.go_to_group(group.permalink)
+                try:
+                    for group in FacebookGroup.objects.filter(active=True, facebook_user=user):
+                        facebook_entries = facebook.go_to_group(group.permalink)
 
-                    for entry in facebook_entries:
-                        # Dodanie postu
-                        post, created = FacebookPost.objects.get_or_create(
-                            post_id=entry['post_id'],
-                            message=entry['post_message'],
-                            date=datetime.fromtimestamp(float(entry['post_date']), tz=pytz.UTC),
-                            author=entry['post_author'],
-                            permalink=entry['post_permalink'],
-                            parent_group=group,
-                        )
+                        for entry in facebook_entries:
+                            # Dodanie postu
+                            post, created = FacebookPost.objects.get_or_create(
+                                post_id=entry['post_id'],
+                                message=entry['post_message'],
+                                date=datetime.fromtimestamp(float(entry['post_date']), tz=pytz.UTC),
+                                author=entry['post_author'],
+                                permalink=entry['post_permalink'],
+                                parent_group=group,
+                            )
 
-                        if entry['post_comments']:
-                            # Dodanie komentarzy z lvl 0
-                            for comment_lvl0 in entry['post_comments']:
-                                comment, created_comment = FacebookPostCommentLvl0.objects.get_or_create(
-                                    comment_id=comment_lvl0['comment_id'],
-                                    author=comment_lvl0['author_name'],
-                                    link_profile=comment_lvl0['author_link_profile'],
-                                    date=datetime.fromtimestamp(float(comment_lvl0['comment_date']), tz=pytz.UTC),
-                                    message=comment_lvl0['comment_text'],
-                                    post=post,
-                                )
+                            if entry['post_comments']:
+                                # Dodanie komentarzy z lvl 0
+                                for comment_lvl0 in entry['post_comments']:
+                                    comment, created_comment = FacebookPostCommentLvl0.objects.get_or_create(
+                                        comment_id=comment_lvl0['comment_id'],
+                                        author=comment_lvl0['author_name'],
+                                        link_profile=comment_lvl0['author_link_profile'],
+                                        date=datetime.fromtimestamp(float(comment_lvl0['comment_date']), tz=pytz.UTC),
+                                        message=comment_lvl0['comment_text'],
+                                        post=post,
+                                    )
 
-                                if comment_lvl0['subcomments']:
-                                    # Dodanie komentarzy z lvl 1
-                                    for comment_lvl1 in comment_lvl0['subcomments']:
-                                        comment1, created_lvl1 = FacebookPostCommentLvl1.objects.get_or_create(
-                                            comment_lvl0=comment,
-                                            comment_id=comment_lvl1['comment_id'],
-                                            author=comment_lvl1['author_name'],
-                                            link_profile=comment_lvl1['author_link_profile'],
-                                            date=datetime.fromtimestamp(float(comment_lvl1['comment_date']), tz=pytz.UTC),
-                                            message=comment_lvl1['comment_text'],
-                                        )
-                facebook.close()
+                                    if comment_lvl0['subcomments']:
+                                        # Dodanie komentarzy z lvl 1
+                                        for comment_lvl1 in comment_lvl0['subcomments']:
+                                            comment1, created_lvl1 = FacebookPostCommentLvl1.objects.get_or_create(
+                                                comment_lvl0=comment,
+                                                comment_id=comment_lvl1['comment_id'],
+                                                author=comment_lvl1['author_name'],
+                                                link_profile=comment_lvl1['author_link_profile'],
+                                                date=datetime.fromtimestamp(float(comment_lvl1['comment_date']), tz=pytz.UTC),
+                                                message=comment_lvl1['comment_text'],
+                                            )
+                    facebook.close()
 
-            except TimeoutException:
-                facebook.close()
-            except NoSuchWindowException:
-                facebook.close()
-            except Exception as e:
-                Sentry.capture_exception(e)
-                facebook.close()
+                except TimeoutException as e:
+                    Sentry.capture_exception(e)
+                    facebook.close()
+                except NoSuchWindowException as e:
+                    Sentry.capture_exception(e)
+                    facebook.close()
+                except Exception as e:
+                    Sentry.capture_exception(e)
+                    facebook.close()
+        except Exception as ex:
+            Sentry.capture_exception(ex)
